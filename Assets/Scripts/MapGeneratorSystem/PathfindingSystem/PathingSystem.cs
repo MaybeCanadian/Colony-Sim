79276223a63c875a22afbbae33f45a0a2c8f7af1
MaybeCanadian <1coffeye@gmail.com>
2023-01-28 04:09:15 -0500

    created some pathing system scripts to start the pathing system

---
+++ Assets/Scripts/MapGeneratorSystem/PathfindingSystem/PathingSystem.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public static class PathingSystem
+{
+    public static PathingPath GetPathFromNodeToNode(PathFindingNode start, PathFindingNode end)
+    {
+        PathingPath path = new PathingPath();
+
+
+
+        return path;
+    }
+
+    public static PathingPath GetPathFromPosToNode(Vector3 start, PathFindingNode end)
+    {
+        PathingPath path = new PathingPath();
+
+        return path;
+    }
+
+    public static PathingPath GetPathFromNodeToPos(PathFindingNode start, Vector3 end)
+    {
+        PathingPath path = new PathingPath();
+
+        return path;
+    }
+
+    public static PathingPath GetPathFromPosToPos(Vector3 start, Vector3 end)
+    {
+        PathingPath path = new PathingPath();
+
+        return path;
+    }
+}
